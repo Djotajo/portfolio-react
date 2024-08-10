10c19d981a47c414b540b191cc6ab3718d3890c8
Djotajo <20823564+Djotajo@users.noreply.github.com>
2024-08-10 15:35:37 +0200

    Remove ; in Footer, Fix padding top on Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./header";
 import Content from "./content";
+import Footer from "./footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Header />
       </header>
       <Content />
+      <Footer />
     </>
   );
 }
